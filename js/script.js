2ca729ee10e0c81415209b0ef317c8ff4b2cef88
Khai <kd.kidr@gmail.com>
2020-06-28 11:16:45 +0700

    update UI

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ const DEBUG = false;
 const usernameInput = document.getElementById('username-input');
 const searchForm = document.getElementById('search-form');
 const backButton = document.getElementById('back-btn');
-
 async function fetchUsers(searchQuery) {
     if (!searchQuery) return;
     const url = `https://api.github.com/search/users?q=${searchQuery}`
@@ -33,7 +32,6 @@ async function fetchRepos(username) {
     const url = `https://api.github.com/users/${username}/repos`
     const response = await fetch(url);
     const result = await response.json();
-
     document.getElementById('repos-list').innerHTML = '';
 
     if (result.message) return;
@@ -42,14 +40,21 @@ async function fetchRepos(username) {
 }
 
 function displayRepo(data) {
-    const { name, created_at, html_url } = data;
+    let { name, created_at, html_url, description, avatar_url,updated_at } = data;
+    created_at = parseDate(created_at);
+    updated_at = parseDate(updated_at);
+    const color = getRandomTailwindColor();
+    const opacity = getRandomTailwindOpacity();
+    if (!description) description = "No description.";
     document.getElementById('repos-list').innerHTML +=
         `
-    <div class='p-10 py-24 text-center w-full md:w-1/2 lg:w-1/3 bg-orange-100 text-xl p-4 border-orange-200 border'>
+    <div class='p-10 py-24 text-center w-full md:w-1/2 lg:w-1/3 bg-${color}-${opacity} text-xl p-4 border-gray-400 border'>
         <a href='${html_url}' target='_blank'><img class='mx-4 mb-4 w-16 mx-auto rounded-full' src='images/github1.png'>
             <div>
                 <h3 class='text-2xl mb-2'>${name}</h3>
-                <h3 class='text-sm'>${parseDate(created_at)}</h3>
+                <h3 class='text-sm mb-2'>${description}</h3>
+                <h3 class='text-sm mb-2'>Created at: ${created_at}</h3>
+                <h3 class='text-sm'>Last active: ${updated_at}</h3>
             </div>
         </a>
     </div>
@@ -98,12 +103,22 @@ function displayOnly(displayId) {
     if (visibleElement)
         visibleElement.classList.remove('hidden');
 }
-
+function getRandomTailwindColor() {
+    const colors = ['red', 'blue', 'gray', 'pink', 'teal', 'green', 'indigo', 'orange', 'purple', 'yellow'];
+    const random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
+function getRandomTailwindOpacity(){
+    const opacity = [100,200]
+    const random = Math.floor(Math.random() * opacity.length);
+    return opacity[random];
+}
 // MAIN 
+if (!DEBUG) { displayOnly('') }
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const searchQuery = usernameInput.value
     fetchUsers(searchQuery);
 })
 backButton.addEventListener('click', () => { displayOnly('users') });
-if (!DEBUG) { displayOnly('') }
+
